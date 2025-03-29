b86da00d11dc1ccf07724cda3e2f5130728ace7d
Akshay Pai <akshaypaik@gmail.com>
2025-03-29 18:47:05 +0530

    adding module category for topics and displaying it on UI

--- src/components/CoursePage/CoursePage.jsx
+++ src/components/CoursePage/CoursePage.jsx
@@ -11,12 +11,26 @@ const CoursePage = () => {
   const [courseInfo, setCourseInfo] = useState({});
   const courseName = useSelector((store) => store.course.currentCourse);
   const dispatch = useDispatch();
+  const [courseByModule, setCourseByModule] = useState([]);
 
   const fetchCourseDetails = async () => {
     const result = await fetch(`http://localhost:3000/BackendApp/api/courses/getCourseDetailsByCourseId?courseId=${courseId}`);
     const resultJson = await result.json();
     console.log("resultJson: ", resultJson);
     setCourseInfo(resultJson);
+ 
+    const moduleCategories = new Set([]);
+    resultJson?.topicsInfo?.forEach((topic) => {
+      moduleCategories.add(topic.module);
+    });
+
+    const updatedCourseByModule = [];
+    moduleCategories.forEach((moduleCategory) => {
+      const topicModuleInfos = resultJson?.topicsInfo?.filter((topic) => topic.module === moduleCategory);
+      updatedCourseByModule.push({ module: moduleCategory, topicsInfo: topicModuleInfos });
+    });
+    setCourseByModule(updatedCourseByModule);
+    console.log("courseByModule: ", courseByModule);
   }
 
   useEffect(() => {
@@ -37,7 +51,13 @@ const CoursePage = () => {
       </div>
       <p>{courseInfo?.basicInfo?.overviewText}</p>
       <div className='topic-card-course-container'>
-        {courseInfo?.topicsInfo?.map((topic) => <TopicCard key={topic.topicName} topicInfo={topic} />)}
+        {courseByModule.length > 0 && courseByModule?.map((module) =>
+          <div key={module.module} className='module-course-container'>
+            <h2>{module.module.charAt(0).toUpperCase() + module.module.slice(1)}</h2>
+            <div className='module-course-list'>
+              {module.topicsInfo.map((topic) => <TopicCard key={topic.topicName} topicInfo={topic} />)}
+            </div>
+          </div>)}
       </div>
     </div>
   )
