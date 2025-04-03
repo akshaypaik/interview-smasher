507d6e56f8eb4ce2fdff4dd2650aad804798fcc7
Akshay Pai <akshaypaik@gmail.com>
2025-04-03 23:43:46 +0530

    searchbar functionality added to favorite companies page

--- src/components/Interviews/FavoriteCompanies/FavoriteCompanies.jsx
+++ src/components/Interviews/FavoriteCompanies/FavoriteCompanies.jsx
@@ -2,30 +2,39 @@ import React, { useEffect, useState } from 'react';
 import './FavoriteCompanies.css';
 import { GET_FAVORITE_COMPANIES_INTERVIEW } from '../../../utils/constants/apiConstants';
 import CompanyCard from '../CompanyCard/CompanyCard';
+import { useSelector } from 'react-redux';
 
 const FavoriteCompanies = () => {
 
     const [favoriteCompanies, setFavoriteCompanies] = useState([]);
+    const [filteredFavoriteCompanies, setFilteredFavoriteCompanies] = useState([]);
+    const searchBarQuery = useSelector((store) => store.app.searchBarQuery);
 
     const fetchFavCompanies = async () => {
         const userEmail = "akshaypaik@gmail.com"
         const result = await fetch(`${GET_FAVORITE_COMPANIES_INTERVIEW}${userEmail}`);
         const resultJson = await result.json();
         setFavoriteCompanies(resultJson);
+        setFilteredFavoriteCompanies(resultJson);
     }
 
     useEffect(() => {
         fetchFavCompanies();
     }, []);
 
+    useEffect(() => {
+        const filtered = favoriteCompanies.filter((company) => company.name.includes(searchBarQuery) || company.displayName.includes(searchBarQuery));
+        setFilteredFavoriteCompanies(filtered);
+    }, [searchBarQuery]);
+
     return (
         <div className='favorite-companies-container'>
             <h1>Your Favorite Companies</h1>
-            {favoriteCompanies.length === 0 &&
+            {filteredFavoriteCompanies.length === 0 &&
                 <div className='interview-no-result-text'> Please add favorite companies in Interview section...
                 </div>}
             <div className='company-card-main-container'>
-                {favoriteCompanies.length > 0 && favoriteCompanies.map((company) => <CompanyCard key={company.companyId} info={company} />)}
+                {filteredFavoriteCompanies.length > 0 && filteredFavoriteCompanies.map((company) => <CompanyCard key={company.companyId} info={company} />)}
             </div>
         </div>
     )
