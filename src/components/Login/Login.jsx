dc11c8e668f522e80b3bdee536f79ba8df23903e
Akshay Pai <akshaypaik@gmail.com>
2025-04-07 09:58:37 +0530

    login form validations using react hook form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,7 +4,10 @@ import { useForm } from 'react-hook-form';
 
 const Login = () => {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState } = useForm();
+    const { errors } = formState;
+    console.log(errors);
+
 
     const handleLoginSubmit = (data) => {
         if (data) {
@@ -16,17 +19,37 @@ const Login = () => {
         }
     }
 
+    const handleError = (errors) => {
+        // console.log(errors);
+    }
+
     return (
         <div className='login-container'>
             <h2>Login</h2>
-            <form className='login-form' onSubmit={handleSubmit(handleLoginSubmit)}>
+            <form className='login-form' onSubmit={handleSubmit(handleLoginSubmit, handleError)}>
                 <div className='login-field'>
-                    <label>Email</label>
-                    <input type='text' placeholder='Email' id='email' {...register("email")} />
+                    <label htmlFor='email'>Email</label>
+                    <input type='text' placeholder='Email' id='email' {...register("email", {
+                        required: "This field is required",
+                        validate: (value) => {
+                            const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(value);
+                            return isEmailValid || "Email ID is not valid";
+                        }
+                    })} />
+                    {errors?.email?.message &&
+                        <span className='error-msg'>{errors?.email?.message}</span>}
                 </div>
                 <div className='login-field'>
-                    <label>Password</label>
-                    <input type='password' placeholder='Password' id='password' {...register("password")} />
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' placeholder='Password' id='password' {...register("password", {
+                        required: "This field is required",
+                        validate: (value) => {
+                            const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(value);
+                            return isPasswordValid || "Password is not valid. Please provide one uppercase, one special character and one number and a minimum of eight characters";
+                        }
+                    })} />
+                    {errors?.password?.message &&
+                        <span className='error-msg'>{errors?.password?.message}</span>}
                 </div>
                 <button onClick={(e) => handleLoginSubmit(e)}>Submit</button>
             </form>
