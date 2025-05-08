2d5e9e916a7b1c56b6767a4ca121f39589dc7cba
Akshay Pai <akshaypaik@gmail.com>
2025-05-08 12:11:31 +0530

    reusable component for quick filter sections in career link page

--- src/components/QuickCareerLinks/QuickCareerLinksFilters.jsx
+++ src/components/QuickCareerLinks/QuickCareerLinksFilters.jsx
@@ -8,30 +8,32 @@ const QuickCareerLinksFilters = ({ info, resetQuickFilterRolesAndLocations, setR
     const dispatch = useDispatch();
     const [roles, setRoles] = useState([]);
     const [locations, setLocations] = useState([]);
+    const [companies, setCompanies] = useState([]);
     const isResetApplied = useRef(false);
 
     useEffect(() => {
-        if(resetQuickFilterRolesAndLocations){
+        if (resetQuickFilterRolesAndLocations) {
             setRoles([]);
             setLocations([]);
             setResetQuickFilterRolesAndLocations(false);
             isResetApplied.current = true;
             setRolesAndLocation();
-        }else{
+        } else {
             isResetApplied.current = false;
         }
     }, [resetQuickFilterRolesAndLocations, info]);
 
     useEffect(() => {
-       setRolesAndLocation();
+        setRolesAndLocation();
     }, [info]);
 
     const setRolesAndLocation = () => {
         if (info && Array.isArray(info) && ((roles.length === 0 && locations.length === 0) || isResetApplied.current)) {
             const locationArray = [...new Set(info.map(jobEntry => jobEntry.jobLocation))];
             const roleArray = [...new Set(info.map(jobEntry => jobEntry.jobRole))];
+            const companyArray = [...new Set(info.map(company => company.company))];
             const locationArrayObject = [];
-            for(let location of locationArray){
+            for (let location of locationArray) {
                 locationArrayObject.push({
                     id: Math.floor(100000 + Math.random() * 900000),
                     name: location,
@@ -39,15 +41,24 @@ const QuickCareerLinksFilters = ({ info, resetQuickFilterRolesAndLocations, setR
                 })
             }
             const roleArrayObject = [];
-            for(let role of roleArray){
+            for (let role of roleArray) {
                 roleArrayObject.push({
                     id: Math.floor(100000 + Math.random() * 900000),
                     name: role,
                     displayName: role
                 });
             }
+            const companyArrayObject = [];
+            for (let company of companyArray) {
+                companyArrayObject.push({
+                    id: Math.floor(100000 + Math.random() * 900000),
+                    name: company,
+                    displayName: company
+                });
+            }
             setLocations(locationArrayObject);
             setRoles(roleArrayObject);
+            setCompanies(companyArrayObject);
         }
     }
 
@@ -62,43 +73,41 @@ const QuickCareerLinksFilters = ({ info, resetQuickFilterRolesAndLocations, setR
     return (
         <div className='filter-container w-84 bg-neutral-100 p-4 rounded-xl shadow-2xl border-2 dark:bg-gray-700 max-h-[70vh] overflow-y-scroll'>
             <div>
-                <h4 className='font-bold text-[14px]'>STATUS</h4>
-                {quickFilterCareerLinkOptions.map((status) => {
-                    return <span className='flex gap-2 m-1' key={status.id}>
-                        <input type='checkbox' id={status.id} onClick={(e) => {
-                            e.stopPropagation();
-                            handleQuickCareerLinkClick(status, "jobStatus");
-                        }} className='cursor-pointer' />
-                        <label htmlFor={status.id} className='cursor-pointer text-[14px]'>{status.displayName}</label>
-                    </span>
-                })}
+                <QuickCareerLinksFilterSection sectionName="STATUS" filterEntries={quickFilterCareerLinkOptions}
+                    category="jobStatus"
+                    handleQuickCareerLinkClick={handleQuickCareerLinkClick} />
             </div>
             <div>
-                <h4 className='font-bold text-[14px]'>LOCATION</h4>
-                {locations.map((location) => {
-                    return <span className='flex gap-2 m-1' key={location.id}>
-                        <input type='checkbox' id={location.id} onClick={(e) => {
-                            e.stopPropagation();
-                            handleQuickCareerLinkClick(location, "jobLocation");
-                        }} className='cursor-pointer' />
-                        <label htmlFor={location.id} className='cursor-pointer text-[14px]'>{location.displayName}</label>
-                    </span>
-                })}
+                <QuickCareerLinksFilterSection sectionName="LOCATION" filterEntries={locations} category="jobLocation"
+                    handleQuickCareerLinkClick={handleQuickCareerLinkClick} />
             </div>
             <div>
-                <h4 className='font-bold text-[14px]'>ROLES</h4>
-                {roles.map((role) => {
-                    return <span className='flex gap-2 m-1' key={role.id}>
-                        <input type='checkbox' id={role.id} onClick={(e) => {
-                            e.stopPropagation();
-                            handleQuickCareerLinkClick(role, "jobRole");
-                        }} className='cursor-pointer' />
-                        <label htmlFor={role.id} className='cursor-pointer text-[14px]'>{role.displayName}</label>
-                    </span>
-                })}
+                <QuickCareerLinksFilterSection sectionName="ROLES" filterEntries={roles} category="jobRole"
+                    handleQuickCareerLinkClick={handleQuickCareerLinkClick} />
+            </div>
+            <div>
+                <QuickCareerLinksFilterSection sectionName="COMPANIES" filterEntries={companies} category="company"
+                    handleQuickCareerLinkClick={handleQuickCareerLinkClick} />
             </div>
         </div>
     )
 }
 
 export default QuickCareerLinksFilters;
+
+export function QuickCareerLinksFilterSection({ sectionName, filterEntries, category, handleQuickCareerLinkClick }) {
+    return (
+        <>
+            <h4 className='font-bold text-[14px]'>{sectionName}</h4>
+            {filterEntries.map((entry) => {
+                return <span className='flex gap-2 m-1' key={entry.id}>
+                    <input type='checkbox' id={entry.id} onClick={(e) => {
+                        e.stopPropagation();
+                        handleQuickCareerLinkClick(entry, category);
+                    }} className='cursor-pointer' />
+                    <label htmlFor={entry.id} className='cursor-pointer text-[14px]'>{entry.displayName}</label>
+                </span>
+            })}
+        </>
+    )
+}
