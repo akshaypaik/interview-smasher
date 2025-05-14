dcdec24b2cb308d4bd4e76d542a7a2c2bff94276
Akshay Pai <akshaypaik@gmail.com>
2025-05-14 10:48:26 +0530

    small bug fix of highlighting home sidebar

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './Logo.css';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { openSideBar } from '../../../utils/ReduxStore/appSlice';
+import { openSideBar, setCurrentSidebarTab } from '../../../utils/ReduxStore/appSlice';
 
 const Logo = () => {
 
   const dispatch = useDispatch();
 
   const handleLogoClick = () => {
+    dispatch(setCurrentSidebarTab("home"));
     dispatch(openSideBar());
   }
 
