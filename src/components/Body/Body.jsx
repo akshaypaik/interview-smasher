647856765e9db8eb2e46c976286017b6a92c98c4
Akshay Pai <akshaypaik@gmail.com>
2025-05-30 20:12:55 +0530

    smooth opening of login sidebar

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -6,6 +6,7 @@ import Header from '../Header/Header';
 import './Body.css';
 import { Toaster } from 'react-hot-toast';
 import Login from '../Login/Login';
+import { AnimatePresence, motion } from "framer-motion";
 
 const Body = () => {
 
@@ -34,7 +35,16 @@ const Body = () => {
             <Header />
             <div className='body-container pt-16'>
                 {showSidebar && userInfo?.email && <Sidebar />}
-                {showLoginSidebar && <Login />}
+                {showLoginSidebar && (
+                    <motion.div
+                        initial={{ x: "100%" }}        
+                        animate={{ x: 0 }}            
+                        exit={{ x: "100%" }}          
+                        transition={{ duration: 0.3, ease: "easeOut" }}
+                        className="fixed right-0 top-0 h-full w-96 z-50">
+                        <Login />
+                    </motion.div>
+                )}
                 <Outlet />
             </div>
         </>
